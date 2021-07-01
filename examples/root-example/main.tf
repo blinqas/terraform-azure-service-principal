839a8d3c441977f2a9426e42800e6bbf21d4f6ac
Kim Iversen <kim.iversen@blinq.no>
2021-07-01 09:23:42 +0200

    Updated example to use Key Vault

--- examples/root-example/main.tf
+++ examples/root-example/main.tf
@@ -5,9 +5,15 @@ provider "azurerm" {
 module "service-principal" {
     source = "../../"
     name   = "sp-tf-${var.name}"
+    key_vault_id = var.key_vault_id
 }
 
 variable "name" {
   type = string
   description = "The name of the service principal"
+}
+
+variable "key_vault_id" {
+  type = string
+  description = "The ID of the Key Vault to store credentials"
 }

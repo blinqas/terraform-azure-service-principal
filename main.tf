36b61f2e9783907508df8144b2fb6608f01c2bb2
Kim Iversen <kim.iversen@blinq.no>
2022-06-07 12:10:12 +0000

    Merged PR 4: Module refactor
    Removed jobs that aren't for this module to perform. Added option to provide rotating password. Updated terraform modules. This is version four.

--- main.tf
+++ main.tf
@@ -2,11 +2,12 @@ terraform {
   required_providers {
     azuread = {
       source  = "hashicorp/azuread"
-      version = "1.6.0"
+      version = "2.21.0"
     }
+
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.65.0"
+      version = "3.3.0"
     }
   }
 }
@@ -19,7 +20,7 @@ data "azurerm_client_config" "current" {}
 
 # Create the underlying application for the service principal.
 resource "azuread_application" "app" {
-  display_name = var.name
+  display_name = var.app_display_name
   identifier_uris = [
     format("http://%s.%s", var.app_name, var.identifier_uri_verified_domain)
   ]
@@ -30,40 +31,17 @@ resource "azuread_service_principal" "sp" {
   application_id = azuread_application.app.application_id
 }
 
+# Rotate this resource at time of variable value
+resource "time_rotating" "sp" {
+  rotation_days = var.password_rotating_days
+}
+
 # Set a random password for the service principal.
 resource "azuread_service_principal_password" "pw" {
   service_principal_id = azuread_service_principal.sp.object_id
-}
-
-# Retrieve the data for the user specified role.
-data "azurerm_role_definition" "main" {
-  count = var.role == "" ? 0 : 1
-  name  = var.role
-}
-
-# Assign user specified role(s) to the service principal.
-resource "azurerm_role_assignment" "main" {
-  count              = var.role == "" ? 0 : length(var.scopes)
-  scope              = var.scopes[count.index]
-  role_definition_id = format("%s%s", data.azurerm_subscription.current.id, data.azurerm_role_definition.main[0].id)
-  principal_id       = azuread_service_principal.sp.id
-}
-
-# Temporary locals
-locals {
-  is_kv_enabled = var.key_vault_id != ""
-  values_to_store = {
-    client-id       = azuread_application.app.application_id
-    client-secret   = azuread_service_principal_password.pw.value
-    tenant-id       = data.azurerm_client_config.current.tenant_id
-    subscription-id = data.azurerm_subscription.current.subscription_id
+  end_date_relative    = var.end_date_relative_hours
+  rotate_when_changed = {
+    rotation = time_rotating.sp.id
   }
 }
 
-# Store service principal credentials in the provided Key Vault
-resource "azurerm_key_vault_secret" "secret" {
-  count = local.is_kv_enabled ? length(local.values_to_store) : 0
-  key_vault_id = var.key_vault_id
-  name = format("%s%s", "kv-arm-", keys(local.values_to_store)[count.index])
-  value = format("%s%s", "kv-arm-", lookup(local.values_to_store, keys(local.values_to_store)[count.index]))
-}
--- variables.tf
+++ variables.tf
@@ -1,32 +1,27 @@
-variable "name" {
-  type = string
+variable "app_display_name" {
+  type        = string
   description = "The name of the service principal"
 }
 
 variable "app_name" {
-  type = string
+  type        = string
   description = "The name of the application (normally the project name)"
 }
 
-variable "role" {
-  type = string
-  description = "The name of a role for the service principal. Does nothing if var.scopes isn't set."
-  default = ""
-}
-
-variable "scopes" {
-  type = list(string)
-  description = "A list of scopes the role assignment applies to. Defaults to []"
-  default = []
-}
-
-variable "key_vault_id" {
-  type = string
-  description = "The ID of the Key Vault to store service principal credentials. Do not use a key vault that generates an access policy assigned to the this service principal. Causes a dependency loop. Defaults to \"\""
-  default = ""
-}
-
 variable "identifier_uri_verified_domain" {
-  type = string
+  type        = string
   description = "Change since October 2021 requires a verified domain of the organization or its subdomain. Example: wwan.no"
 }
+
+variable "password_rotating_days" {
+  type        = number
+  description = "Number in days the password for the service principal should rotate. When this number is reached, the password resource is destroyed and recreated. It requires Terraform to be run at all for any change to occur."
+  default     = 7
+}
+
+variable "end_date_relative_hours" {
+  type        = string
+  description = "Relative day from today that the service principal password expires."
+  default     = "168h"
+}
+
